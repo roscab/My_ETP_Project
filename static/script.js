633ca74453972b474517336cc3055a2c655459c4
robo <bogdan.rosca@erni-espana.es>
2018-10-21 20:35:13 +0200

    added: Question filer

--- static/script.js
+++ static/script.js
@@ -1,6 +1,61 @@
-$(document).ready(function(){
-    $("#btn_report").click(function(){
+var subjectGroups = ['ISTQB','CAN','OOP','Personal','Logic'];
 
+$(document).ready(function(){
+    $("#btn_istqb").click(function(){
+        for (var i = 0; i < 5; i++){
+            var classToHide = '.' + subjectGroups[i];
+            $(classToHide).hide();        
+        }
+        $('.ISTQB').show();   
+    });
+});
+
+$(document).ready(function(){
+    $("#btn_can").click(function(){
+        for (var i = 0; i < 5; i++){
+            var classToHide = '.' + subjectGroups[i];
+            $(classToHide).hide();        
+        }
+        $('.CAN').show();   
+    });
+});
+
+$(document).ready(function(){
+    $("#btn_oop").click(function(){
+        for (var i = 0; i < 5; i++){
+            var classToHide = '.' + subjectGroups[i];
+            $(classToHide).hide();        
+        }
+        $('.OOP').show();   
+    });
+});
+
+$(document).ready(function(){
+    $("#btn_logic").click(function(){
+        for (var i = 0; i < 5; i++){
+            var classToHide = '.' + subjectGroups[i];
+            $(classToHide).hide();        
+        }
+        $('.Logic').show();   
+    });
+});
+
+$(document).ready(function(){
+    $("#btn_personal").click(function(){
+        for (var i = 0; i < 5; i++){
+            var classToHide = '.' + subjectGroups[i];
+            $(classToHide).hide();        
+        }
+        $('.Personal').show();   
+    });
+});
+
+$(document).ready(function(){
+    $("#btn_all").click(function(){
+        for (var i = 0; i < 5; i++){
+            var classToShow= '.' + subjectGroups[i];
+            $(classToShow).show();        
+        }
     });
 });
 
