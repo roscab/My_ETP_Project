fc000a1eb6c999e71cc657d220fc736ab88b3238
robo <bogdan.rosca@erni-espana.es>
2018-10-27 15:27:12 +0200

    Modified functions and added selected feature.

--- static/script.js
+++ static/script.js
@@ -1,61 +1,109 @@
-var subjectGroups = ['ISTQB','CAN','OOP','Personal','Logic'];
 
+// This needs to be reduced
 $(document).ready(function(){
-    $("#btn_istqb").click(function(){
-        for (var i = 0; i < 5; i++){
-            var classToHide = '.' + subjectGroups[i];
-            $(classToHide).hide();        
-        }
-        $('.ISTQB').show();   
+    $("#btn_istqb").click(function(){    
+            var questionType = $(this).attr("tag");
+            console.log( "button tag=" + questionType );
+
+            $(".question").each(function( index ) {
+                $(this).hide();
+              });  
+            $(".question").each(function( index ) {
+                if($(this).attr("tag") ==  questionType)
+                     $(this).show();
+              });        
     });
 });
 
 $(document).ready(function(){
-    $("#btn_can").click(function(){
-        for (var i = 0; i < 5; i++){
-            var classToHide = '.' + subjectGroups[i];
-            $(classToHide).hide();        
-        }
-        $('.CAN').show();   
+    $("#btn_can").click(function(){    
+            var questionType = $(this).attr("tag");
+
+            $(".question").each(function( index ) {
+                $(this).hide();
+              });  
+            $(".question").each(function( index ) {
+                if($(this).attr("tag") ==  questionType)
+                     $(this).show();
+              });        
     });
 });
 
 $(document).ready(function(){
-    $("#btn_oop").click(function(){
-        for (var i = 0; i < 5; i++){
-            var classToHide = '.' + subjectGroups[i];
-            $(classToHide).hide();        
-        }
-        $('.OOP').show();   
+    $("#btn_oop").click(function(){    
+            var questionType = $(this).attr("tag");
+
+            $(".question").each(function( index ) {
+                $(this).hide();
+              });  
+            $(".question").each(function( index ) {
+                if($(this).attr("tag") ==  questionType)
+                     $(this).show();
+              });        
     });
 });
 
 $(document).ready(function(){
-    $("#btn_logic").click(function(){
-        for (var i = 0; i < 5; i++){
-            var classToHide = '.' + subjectGroups[i];
-            $(classToHide).hide();        
-        }
-        $('.Logic').show();   
+    $("#btn_logic").click(function(){    
+            var questionType = $(this).attr("tag");
+
+            $(".question").each(function( index ) {
+                $(this).hide();
+              });  
+            $(".question").each(function( index ) {
+                if($(this).attr("tag") ==  questionType)
+                     $(this).show();
+              });        
     });
 });
 
 $(document).ready(function(){
-    $("#btn_personal").click(function(){
-        for (var i = 0; i < 5; i++){
-            var classToHide = '.' + subjectGroups[i];
-            $(classToHide).hide();        
-        }
-        $('.Personal').show();   
+    $("#btn_personal").click(function(){    
+            var questionType = $(this).attr("tag");
+
+            $(".question").each(function( index ) {
+                $(this).hide();
+              });  
+            $(".question").each(function( index ) {
+                if($(this).attr("tag") ==  questionType)
+                     $(this).show();
+              });        
     });
 });
+
+// This needs to be reduced
 
 $(document).ready(function(){
-    $("#btn_all").click(function(){
-        for (var i = 0; i < 5; i++){
-            var classToShow= '.' + subjectGroups[i];
-            $(classToShow).show();        
-        }
+    $("#btn_all").click(function(){            
+            $(".question").each(function( index ) {
+                $(this).show();
+              });     
     });
 });
 
+// $(document).ready(function(){
+//     var counter = 0;
+//     $("#btn_selected").click(function(){            
+//             $(".question").each(function( index ) {
+//                 $(this).hide();
+//               });    
+//             $("input").each(function( index ) {
+//                 if ($($(this)).is(":checked")) 
+//                  counter++;
+//                  $(this).parent().show();
+//               });     
+//               console.log ( "selected=" + counter);  
+//     });
+// });
+
+$(document).ready(function(){
+    $("#btn_selected").click(function(){            
+            $(".question").each(function( index ) {
+                $(this).hide();
+              });    
+            $(".question").each(function( index ) {
+                if ($($(this).children()).is(":checked"))
+                $(this).show();  
+              });     
+    });
+});
